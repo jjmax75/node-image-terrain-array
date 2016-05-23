3d891bd9654bc61644dbaa61c131f0640b0120f7
John Behan <behanj@gmail.com>
2016-05-23 14:26:37 +0700

    sperated to own module

---
+++ README.md
@@ -0,0 +1,39 @@
+# Node Image to Terrain Array by Box It Off
+
+Input - png file, output filename number of columns, number of rows
+
+Output - array of map terrain
+
+## Overview
+
+This is a module for a bigger project. It is self contained and will output to a file in the output folder. On its own it is not very useful but watch this space :)
+
+## Example
+Turn this -
+
+![An image of part of the world][world]
+
+Into this -
+
+["water","water","water","water","water","water", ...,"land"...,"desert"...,"mountain"]
+
+## Usage
+`npm hexMapGenerator.js <input-file.png> <output-file> <cols> <rows>`
+
+## Further Development
+- output code for rendering map
+- command line inputs
+- Browser gui
+
+## Tasks
+- [x] get image width and height
+- [x] get hex radius
+- [x] get center point of each hex
+- [x] get pixel sample (array->rgba values) function
+- [x] get average colour(array->rgb) of each sample
+- [x] get hsv(array) values from rgb averages
+- [x] convert hsv to terrain type (array)
+- [x] output array to file
+- [x] clean up code :)
+
+[world]: https://github.com/jjmax75/node-image-terrain-array/blob/master/test/resources/map.png "World - Asia, Europe, North Africa"
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = process.cwd();
+
+const imageFile = path + '/' + process.argv[2];
+const outputFile = path + '/output/' + process.argv[3];
+const numCols = Number(process.argv[4]);
+const numRows = Number(process.argv[5]);
+
+const createGridPoints = require(path + '/src/createGridPoints');
+const imageWorker = require(path + '/src/imageWorker');
+const generatorHelper = require(path + '/src/helperFunctions');
+
+const image = imageWorker(imageFile);
+const gridPoints = createGridPoints(image.width, image.height, numCols, numRows);
+const helper = generatorHelper();
+
+// get details for the hexes
+const hexRadius = gridPoints.calculateHexRadius();
+const hexCentres = gridPoints.calculateEachHexCentre(hexRadius);
+
+// get rgba samples values for each hexagon
+helper.getPixels(image)
+.then(function(pixels) {
+  return helper.getSamplePixelsColours(pixels, hexRadius, hexCentres, image);
+})
+.then(function(samples) {
+  return helper.getAverageColours(samples, image);
+})
+.then(function(averageColours) {
+  return helper.convertToHSV(averageColours, image);
+})
+.then(function(hsvValues) {
+  return helper.mapColourToTileType(hsvValues);
+})
+.then(function(terrain) {
+  helper.writeToFile(terrain, outputFile);
+  // TODO refactor to return array
+})
+.catch(function(error) {
+  console.log(error);
+});
